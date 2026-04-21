7d83090561c76df930ac6c09577694ec27737332
mongimassoudi3-gif <mongimassoudi3@gmail.com>
2026-04-21 09:13:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,5 +5,8 @@
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.1.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js" 2>/dev/null || true)
 
 # 无 WiFi 配置：缩小 Q6 内存到 8MB（重要！）
-find ./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ -type f ! -iname '*nowifi*' -exec sed -i 's/ipq$$   6018\|8074   $$\.dtsi/ipq\1-nowifi.dtsi/g' {} + 2>/dev/null || true
+find ./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ \
+  -type f ! -iname '*nowifi*' \
+  -exec sed -i 's/ipq\(6018\|8074\)\.dtsi/ipq\1-nowifi.dtsi/g' {} + 2>/dev/null || true
+
 sed -i 's/0x1000000/0x800000/g' ./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-nowifi.dtsi 2>/dev/null || true
